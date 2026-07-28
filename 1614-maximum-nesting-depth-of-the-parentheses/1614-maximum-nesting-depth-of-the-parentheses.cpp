a1393110983de6204e57b711e2bac99f3ea28d98
Sanskar Gupta <guptasanskar7002@gmail.com>
2026-07-28 16:08:00 +0530

    Time: 0 ms (100%), Space: 8.4 MB (24.67%) - LeetHub

---
+++ 1614-maximum-nesting-depth-of-the-parentheses/1614-maximum-nesting-depth-of-the-parentheses.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int maxDepth(string s) {
+        int ans = 0;
+        int cnt = 0 ;
+        for(char ch : s){
+            if(ch == '('){
+                cnt ++;
+                ans = max(ans,cnt);
+            }
+            else if (ch == ')'){
+                cnt --;
+            }
+        }
+        return ans;
+    }
+};
